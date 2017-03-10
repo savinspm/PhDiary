ba7d6d9cbd2e2f3a12a545bcd5f3dfadf1b1605d
Savíns Puertas Martín <savins93@gmail.com>
2017-03-10 13:48:08 +0100

    First script

---
+++ goodMorning.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import datetime
+import os.path
+import sys
+
+
+
+def ini():
+    if not (os.path.exists("book")):
+        os.mkdir("book")
+    if not(os.path.exists("book/days")):
+        os.mkdir("book/days")
+    if not(os.path.exists("pdf")):
+        os.mkdir("pdf")
+
+def progress():
+    outfile = open('book/main.Rnw', 'w')
+    outfile.write("\\documentclass[a4paper,12pt]{book}\n")
+    outfile.write("\\input{../structure.tex}\n\n")
+    outfile.write("\\begin{document}\n\n")
+    outfile.write("<<setup, include=FALSE, cache=FALSE>>=\n")
+    outfile.write("library(knitr)\n")
+    outfile.write("# set global chunk options\n")
+    outfile.write("opts_chunk$set(cache=FALSE)\n")
+    outfile.write("options(formatR.arrow=TRUE,width=90)\n")
+    outfile.write("@\n\n")
+    outfile.write("\\author{Savíns Puertas Martín}\n")
+    outfile.write("\\title{Cada capítulo es un día distinto}\n")
+    outfile.write("\\date{Start in : 30/11/2016}\n\n")
+    outfile.write("\\frontmatter\n")
+    outfile.write("\\maketitle\n")
+    outfile.write("\\dominitoc% Initialization\n")
+    outfile.write("\\tableofcontents\n")
+    outfile.write("\\mainmatter\n\n")
+    
+    with open("days.txt", "r") as ins:
+        array = []
+        for line in ins:
+            values = line.split()
+            outfile.write("<<{}, child='{}'>>=\n".format(values[0], values[1]))
+            outfile.write("# \input{{{}}}\n".format(values[1]))
+            outfile.write("@\n\n")
+        ins.close()
+
+    
+    outfile.write("\\backmatter\n")
+    outfile.write("% bibliography, glossary and index would go here.\n")
+    outfile.write("% print the full bibliography (using the bibliography style \n")
+    outfile.write("% defined above for the heading above\n")
+    outfile.write("\\end{document}\n")
+    outfile.close()
+
+def hello():
+    i = datetime.datetime.now()
+    nowtime = "{}-{}-{}--{}-{}-{}".format(i.year, i.month, i.day, i.hour, i.minute, i.second)
+    testVar = raw_input("Descriptive name: ")
+    testVar = testVar.title()
+    testVar = testVar.replace(' ', '')
+    with open('days.txt', 'a') as file:
+        file.write("{0} days/{0}--{1}/{0}--{1}.Rnw\n".format(nowtime, testVar))
+        file.close()
+    progress()
+    
+    os.mkdir("book/days/{}--{}".format(nowtime, testVar))
+    with open("book/days/{0}--{1}/{0}--{1}.Rnw".format(nowtime, testVar), "w") as ins:
+        ins.write("\\chapter{{{0}}}\n".format(testVar))
+        ins.write("{{\\centering \\large \\date{{{}}}\par}}\n".format("{}:{} {}/{}/{}".format(i.hour, i.second, i.year, i.month, i.day)))
+        ins.write("\\minitoc% Creating an actual minitoc\n")
+        ins.write("\\vspace{2em}\n")
+    
+def bye():
+    os.chdir("book")
+    print os.getcwd()
+    os.system("R -e \"knitr::knit2pdf('main.Rnw')\" ")
+    
+    os.system("pdflatex main.tex")
+
+def error():
+    print "Ayuda de PROGRAMA"
+    print "Run mode: python file.py [option]"
+    print "[option]:"
+    print "\t ini: Initialize the folders."
+    print "\t hello: Each time you start a new chapter in your diary"
+    print "\t bye: When you have finished and you want to compile the code"
+    print "\t pdf: If you want to move the pdf files to other folders"
+
+if __name__ == "__main__":
+    if not len(sys.argv) == 2:
+        error()
+        sys.exit(0)
+    if not ((sys.argv[1] == "ini") or (sys.argv[1]== "hello") or (sys.argv[1]== "bye") ):
+        error()
+        sys.exit(0)
+    
+    if(sys.argv[1]=="ini"):
+        ini()
+    if(sys.argv[1]=="hello"):
+        hello()
+    if(sys.argv[1]=="bye"):
+        bye()
